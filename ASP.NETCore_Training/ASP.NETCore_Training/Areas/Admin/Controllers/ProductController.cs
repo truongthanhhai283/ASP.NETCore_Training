cf323d97f812829a9e71187f6c88bf344e78bc86
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-20 17:22:15 +0700

    Created list product method

---
+++ ASP.NETCore_Training/ASP.NETCore_Training/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NETCore_Training.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETCore_Training.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var data = _db.Products.Include(c=>c.ProductTypes).Include(c=>c.SpecialTag).ToList();
+            return View(data);
+        }
+    }
+}
