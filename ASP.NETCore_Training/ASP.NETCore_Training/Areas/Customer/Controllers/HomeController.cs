8f528c610ae11cb9aceb843b3de2eab24164e26c
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-21 11:50:05 +0700

    Home_Area -> Created Detail method in Home Page

--- ASP.NETCore_Training/ASP.NETCore_Training/Areas/Customer/Controllers/HomeController.cs
+++ ASP.NETCore_Training/ASP.NETCore_Training/Areas/Customer/Controllers/HomeController.cs
@@ -38,5 +38,50 @@ namespace ASP.NETCore_Training.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //GET product detail acation method
+
+        public ActionResult Detail(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        ////POST product detail acation method
+        //[HttpPost]
+        //[ActionName("Detail")]
+        //public ActionResult ProductDetail(int? id)
+        //{
+        //    List<Products> products = new List<Products>();
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var product = _db.Products.Include(c => c.ProductTypes).FirstOrDefault(c => c.Id == id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    products = HttpContext.Session.Get<List<Products>>("products");
+        //    if (products == null)
+        //    {
+        //        products = new List<Products>();
+        //    }
+        //    products.Add(product);
+        //    HttpContext.Session.Set("products", products);
+        //    return RedirectToAction(nameof(Index));
+        //}
     }
 }
