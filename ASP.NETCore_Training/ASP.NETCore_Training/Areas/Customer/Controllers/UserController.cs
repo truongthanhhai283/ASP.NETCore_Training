c6708ebdd32aa9a7e916b5c7fe47dff589a3d588
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-23 11:27:03 +0700

    ASP.NET Core Identity -> Edit Identity user

--- ASP.NETCore_Training/ASP.NETCore_Training/Areas/Customer/Controllers/UserController.cs
+++ ASP.NETCore_Training/ASP.NETCore_Training/Areas/Customer/Controllers/UserController.cs
@@ -48,5 +48,35 @@ namespace ASP.NETCore_Training.Areas.Customer.Controllers
 
             return View();          
         }
+
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ApplicationUser user)
+        {
+            var userInfo = _db.ApplicationUsers.FirstOrDefault(c => c.Id == user.Id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            userInfo.FirstName = user.FirstName;
+            userInfo.LastName = user.LastName;
+            var result = await _userManager.UpdateAsync(userInfo);
+            if (result.Succeeded)
+            {
+                TempData["save"] = "User has been updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
     }
 }
